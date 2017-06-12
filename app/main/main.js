b18aa7d6c627f892cf9213b997d9e69527ec995f
angkir <angel.kiryazov@nevexis.com>
2017-06-12 17:28:16 +0300

    Adding main page

---
+++ app/main/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular
+    .module('playTogether')
+    .controller('MainCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+        
+        // if ($rootScope.loggedIn == true) {
+        //     document.body.background = "white"; 
+        // }
+
+
+
+    }]);
