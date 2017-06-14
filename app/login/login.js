c79f71b833b4a8f3f1f25dc5213ebc9fc5b6e174
angkir <angel.kiryazov@nevexis.com>
2017-06-14 10:42:47 +0300

    Adding main page

--- app/login/login.js
+++ app/login/login.js
@@ -2,30 +2,32 @@
 
 angular
     .module('playTogether')
-    .controller('LoginCtrl', ['$scope','$state', 'toastr', function ($scope, $state, toastr) {
+    .controller('LoginCtrl', ['$scope', '$state', 'toastr', '$rootScope', function ($scope, $state, toastr, $rootScope) {
 
-        $scope.loginForm = { username: null, password: null};
+        $scope.loginForm = { username: null, password: null };
 
         $scope.confirmLogin = function () {
             var errorMessage = "";
             if ($scope.loginForm.username == null) {
                 errorMessage += "Не сте въвели потребителско име. ";
             }
-            else if ($scope.loginForm.username.length < 8) {
-                errorMessage += "Потребителското име не може да е по-късо от 8 символа. ";
+            else if ($scope.loginForm.username != 'admin') {
+                errorMessage += "Грешно потребителско име. ";
             }
             if ($scope.loginForm.password == null) {
                 errorMessage += "Не сте въвели парола. ";
             }
-            else if ($scope.loginForm.password.length < 8) {
-                    errorMessage += "Паролата не може да е по-къса от 8 символа. "
+            else if ($scope.loginForm.password != 'admin') {
+                errorMessage += "Грешна парола. "
             }
 
             if (errorMessage == "") {
+                $rootScope.loggedIn = true;
                 toastr.success('Успешно влизане в системата!', '');
-                // $state.go('');
+                $state.go('main');
             }
             else {
+                console.log($rootScope.loggedIn);
                 toastr.error(errorMessage, 'Грешка');
             }
 
--- app/main/main.js
+++ app/main/main.js
@@ -3,11 +3,48 @@
 angular
     .module('playTogether')
     .controller('MainCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
-        
-        // if ($rootScope.loggedIn == true) {
-        //     document.body.background = "white"; 
-        // }
 
+        function htmlbodyHeightUpdate() {
+            var height3 = $(window).height();
+            var height1 = $('.nav').height() + 50;
+            var height2 = $('.main').height();
+            if (height2 > height3) {
+                $('html').height(Math.max(height1, height3, height2) + 10);
+                $('body').height(Math.max(height1, height3, height2) + 10);
+            }
+            else {
+                $('html').height(Math.max(height1, height3, height2));
+                $('body').height(Math.max(height1, height3, height2));
+            }
 
+        }
+        $(document).ready(function () {
+            htmlbodyHeightUpdate()
+            $(window).resize(function () {
+                htmlbodyHeightUpdate()
+            });
+            $(window).scroll(function () {
+                height2 = $('.main').height();
+                htmlbodyHeightUpdate();
+            });
+        });
 
+        $scope.oneAtATime = true;
+
+        $scope.events = [{sportType: "Футбол", dateAndTime: "25/04/2017 20:00-21:00", user: "Ангел Кирязов", location: 'ж.к. Дружба, бул. "Цветан Лазаров" 1581, София',
+        peopleGoing: "9/12"}, {sportType: "Футбол", dateAndTime: "25/04/2017 20:00-21:00", user: "Ангел Кирязов", location: 'ж.к. Дружба, бул. "Цветан Лазаров" 1581, София',
+        peopleGoing: "9/12"}];
+
+        $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+        $scope.addItem = function () {
+            var newItemNo = $scope.items.length + 1;
+            $scope.items.push('Item ' + newItemNo);
+        };
+
+        $scope.status = {
+            isCustomHeaderOpen: false,
+            isFirstOpen: true,
+            isFirstDisabled: false
+        };
     }]);
